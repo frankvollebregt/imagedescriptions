4900ec55dddbb165169b2fb70b11e71f89c3dcba
Frank Vollebregt <F.C.J.Vollebregt@student.tudelft.nl>
2021-01-18 14:14:28 +0100

    Adapted imagga file to work again

--- src/imagga.py
+++ src/imagga.py
@@ -2,7 +2,7 @@ import requests
 
 api_key = 'acc_68324598082a188'
 api_secret = 'bb9180ada44fdfff3bf054ded4644003'
-image_path = 'images/bikes.jpg'
+image_path = '../res/img/bikes.jpg'
 
 response = requests.post(
     'https://api.imagga.com/v2/tags',
@@ -12,15 +12,3 @@ res = response.json()
 
 for tag in res['result']['tags']:
     print('{}% {}'.format(round(tag['confidence']), tag['tag']['en']))
-
-# import requests
-#
-# api_key = 'acc_68324598082a188'
-# api_secret = 'bb9180ada44fdfff3bf054ded4644003'
-# image_url = 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
-#
-# response = requests.get(
-#     'https://api.imagga.com/v2/tags?image_url=%s' % image_url,
-#     auth=(api_key, api_secret))
-#
-# print(response.json())
